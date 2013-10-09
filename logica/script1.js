14d429b3fa55de99ae33d490194440e27da29b4a
Christian Eduardo Galdámez Blanco <christianeduardogb@gmail.com>
2013-10-09 00:55:26 -0500

    Añadiendo estilos y funciones JQuery

--- logica/script1.js
+++ logica/script1.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-	
+	var altura = $("#navegacion").offset().top; 
+                         
+      $(window).scroll(function(){
+             
+            if($(window).scrollTop() >= altura){
+                  $("#navegacion").css("margin-top","-510px");
+                  $("#navegacion").css("position","fixed");
+                               
+            }else{
+                  $("#navegacion").css("margin-top","10px");
+                  $("#navegacion").css("position","static");
+                               
+            }
+                         
+      });
 });
